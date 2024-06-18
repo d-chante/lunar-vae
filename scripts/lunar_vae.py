Fix normalization

scripts/lunar_vae.py
@@ -23,80 +23,80 @@ class VAE(nn.Module):
         self.encoder = nn.Sequential(
             # 1e
             Wrap1d(padding=1),
+            # 2e
             nn.ConstantPad1d(
                 padding=3,
                 value=0),
-            # 2e
+            # 3e
             nn.Conv1d(
                 in_channels=1,
                 out_channels=32,
                 kernel_size=3,
                 stride=1,
                 padding=1),
-            # 3e
             nn.BatchNorm1d(32),
             nn.ReLU(),
-            nn.Conv1d(
-                in_channels=32,
-                out_channels=32,
-                kernel_size=2,
-                stride=2,
-                padding=0),
             # 4e
-            nn.BatchNorm1d(32),
-            nn.ReLU(),
             nn.Conv1d(
                 in_channels=32,
                 out_channels=32,
                 kernel_size=2,
                 stride=2,
                 padding=0),
+            nn.BatchNorm1d(32),
+            nn.ReLU(),
             # 5e
-            nn.BatchNorm1d(32),
-            nn.ReLU(),
             nn.Conv1d(
                 in_channels=32,
                 out_channels=32,
                 kernel_size=2,
                 stride=2,
                 padding=0),
+            nn.BatchNorm1d(32),
+            nn.ReLU(),
             # 6e
-            nn.BatchNorm1d(32),
-            nn.ReLU(),
             nn.Conv1d(
                 in_channels=32,
                 out_channels=32,
                 kernel_size=2,
                 stride=2,
                 padding=0),
+            nn.BatchNorm1d(32),
+            nn.ReLU(),
             # 7e
-            nn.BatchNorm1d(32),
-            nn.ReLU(),
             nn.Conv1d(
                 in_channels=32,
                 out_channels=32,
                 kernel_size=2,
                 stride=2,
                 padding=0),
+            nn.BatchNorm1d(32),
+            nn.ReLU(),
             # 8e
-            nn.BatchNorm1d(32),
-            nn.ReLU(),
             nn.Conv1d(
                 in_channels=32,
                 out_channels=32,
                 kernel_size=2,
                 stride=2,
                 padding=0),
+            nn.BatchNorm1d(32),
+            nn.ReLU(),
             # 9e
-            nn.BatchNorm1d(32),
-            nn.ReLU(),
             nn.Conv1d(
                 in_channels=32,
                 out_channels=32,
                 kernel_size=2,
                 stride=2,
                 padding=0),
+            nn.BatchNorm1d(32),
+            nn.ReLU(),
             # 10e
+            nn.Conv1d(
+                in_channels=32,
+                out_channels=32,
+                kernel_size=2,
+                stride=2,
+                padding=0),
             nn.BatchNorm1d(32),
             nn.ReLU())
 
@@ -124,63 +124,63 @@ class VAE(nn.Module):
                 kernel_size=2,
                 stride=2,
                 padding=0),
+            nn.BatchNorm1d(32),
+            nn.ReLU(),
             # 2d
-            nn.BatchNorm1d(32),
-            nn.ReLU(),
             nn.ConvTranspose1d(
                 in_channels=32,
                 out_channels=32,
                 kernel_size=2,
                 stride=2,
                 padding=0),
+            nn.BatchNorm1d(32),
+            nn.ReLU(),
             # 3d
-            nn.BatchNorm1d(32),
-            nn.ReLU(),
             nn.ConvTranspose1d(
                 in_channels=32,
                 out_channels=32,
                 kernel_size=2,
                 stride=2,
                 padding=0),
+            nn.BatchNorm1d(32),
+            nn.ReLU(),
             # 4d
-            nn.BatchNorm1d(32),
-            nn.ReLU(),
             nn.ConvTranspose1d(
                 in_channels=32,
                 out_channels=32,
                 kernel_size=2,
                 stride=2,
                 padding=0),
+            nn.BatchNorm1d(32),
+            nn.ReLU(),
             # 5d
-            nn.BatchNorm1d(32),
-            nn.ReLU(),
             nn.ConvTranspose1d(
                 in_channels=32,
                 out_channels=32,
                 kernel_size=2,
                 stride=2,
                 padding=0),
+            nn.BatchNorm1d(32),
+            nn.ReLU(),
             # 6d
-            nn.BatchNorm1d(32),
-            nn.ReLU(),
             nn.ConvTranspose1d(
                 in_channels=32,
                 out_channels=32,
                 kernel_size=2,
                 stride=2,
                 padding=0),
+            nn.BatchNorm1d(32),
+            nn.ReLU(),
             # 7d
-            nn.BatchNorm1d(32),
-            nn.ReLU(),
             nn.ConvTranspose1d(
                 in_channels=32,
                 out_channels=32,
                 kernel_size=2,
                 stride=2,
                 padding=0),
-            # 8d
             nn.BatchNorm1d(32),
             nn.ReLU(),
+            # 8d
             nn.Conv1d(
                 in_channels=32,
                 out_channels=1,

scripts/train_and_test.py
@@ -10,8 +10,8 @@ import torch
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 from torchsummary import summary
 
-from lunar_vae import VAE
-from utils import Utils
+from scripts.lunar_vae import VAE
+from scripts.utils import Utils
 
 
 def main():
@@ -294,6 +294,8 @@ def main():
             optimizer.param_groups[0]['lr'],
             avg_epoch_time,
             total_training_time,
+            avg_training_loss,
+            avg_validation_loss,
             avg_test_loss,
             m_path)
         logging.info(f"Saved other metrics to {m_path}")

scripts/utils.py
@@ -45,14 +45,10 @@ class Utils(object):
                 profiles.append(p)
 
         data = np.stack(profiles, axis=0)
+        np.random.shuffle(data)
 
         mean = np.mean(data)
         std = np.std(data)
-        data = (data - mean) / std
-
-        data_min = np.min(data)
-        data_max = np.max(data)
-        data = (data - data_min) / (data_max - data_min)
 
         data = np.expand_dims(data, axis=1)
 
@@ -60,19 +56,32 @@ class Utils(object):
             data, test_size=0.1, random_state=42)
 
         train_data, validation_data = train_test_split(
-            remainder_data, test_size=0.2, random_state=42)
-
+            remainder_data, test_size=0.1, random_state=42)
+        
+        train_data = self.Normalize(train_data, mean, std)
         train_tensor = torch.tensor(train_data, dtype=torch.float32)
         train_loader = DataLoader(train_tensor, batch_size, shuffle=True)
 
+        validation_data = self.Normalize(validation_data, mean, std)
         validation_tensor = torch.tensor(validation_data, dtype=torch.float32)
         validation_loader = DataLoader(
-            validation_tensor, batch_size, shuffle=True)
+            validation_tensor, batch_size, shuffle=False)
 
+        test_data = self.Normalize(test_data, mean, std)
         test_tensor = torch.tensor(test_data, dtype=torch.float32)
-        test_loader = DataLoader(test_tensor, batch_size, shuffle=True)
+        test_loader = DataLoader(test_tensor, batch_size, shuffle=False)
 
         return [train_loader, validation_loader, test_loader], [mean, std]
+    
+    @staticmethod
+    def Normalize(data, mean, std):
+        '''
+        TBD
+        '''
+        data_norm = (data - mean) / std
+        data_min = np.min(data_norm)
+        data_max = np.max(data_norm)
+        return (data_norm - data_min) / (data_max - data_min)
 
     @staticmethod
     def GetConfig(config_filepath):
@@ -180,6 +189,8 @@ class Utils(object):
             final_learning_rate,
             average_epoch_time,
             total_training_time,
+            training_loss,
+            validation_loss,
             test_loss,
             filepath):
         with open(filepath, 'w') as file:
@@ -190,4 +201,6 @@ class Utils(object):
             file.write(f"Final learning rate: {final_learning_rate}\n")
             file.write(f"Average epoch time: {average_epoch_time}\n")
             file.write(f"Total training time: {total_training_time}\n")
+            file.write(f"Average training loss: {training_loss}\n")
+            file.write(f"Average validation loss: {validation_loss}\n")
             file.write(f"Test loss: {test_loss}\n")