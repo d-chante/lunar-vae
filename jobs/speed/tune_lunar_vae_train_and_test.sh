0046d2066466014e7f1f0eb453334b247e025e1d
Chantelle G. Dubois <chantelle.dubois@mail.concordia.ca>
2024-06-25 23:22:21 -0400

    Update tuning scripts

--- jobs/speed/tune_lunar_vae_train_and_test.sh
+++ jobs/speed/tune_lunar_vae_train_and_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-config_dir="/home/d/d_chante/d_chante/lunar-vae/config/tuning/4"
+config_dir="/home/d/d_chante/d_chante/lunar-vae/config/tuning/5"
 
 config_files=($(ls ${config_dir}/*.yaml))
 
