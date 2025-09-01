dee11ea340a2b26c708e320b4bbf33f58544d877
Chantelle Dubois <chantelle.dubois@mail.concordia.ca>
2025-08-31 20:46:11 -0400

    Convert numpy float32 to float to avoid type error in visualizelatentspace

--- scripts/lunar_vae.py
+++ scripts/lunar_vae.py
@@ -468,7 +468,7 @@ def VisualizeLatentSpace(
 			with torch.no_grad():
 				# Create a latent vector with one dimension varied
 				z = torch.zeros((1, latent_dim), device=device)
-				z[0, i] = val
+				z[0, i] = float(val)
 
 				prediction = vae.decoder(z.unsqueeze(2))
 				prediction = prediction.squeeze(0).cpu().numpy().reshape(1, -1)
